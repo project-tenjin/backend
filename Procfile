d2f83f4f3551d395e93b621104048bbae71e48e9
Dennis Schmidt <dmrschmidt@gmail.com>
2017-06-26 14:43:12 +0200

    config and folder changes for cloud

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/project-tenjin-1.0-SNAPSHOT.jar
+
--- src/main/java/org/redi_school/attendance/AttendanceWebApplication.java
+++ src/main/java/org/redi_school/attendance/AttendanceWebApplication.java
@@ -1,4 +1,4 @@
-package attendance;
+package org.redi_school.attendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
