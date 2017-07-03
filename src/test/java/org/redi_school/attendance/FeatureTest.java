567c29df79ec497bba0467b5d5e2f0be4397157f
Dennis Schmidt <dschmidt@pivotal.io>
2017-07-03 11:45:17 +0200

    Use phantomjs for feature test [#148068213]
    Signed-off-by: Simon Johansson <sjohansson@pivotal.io>

--- src/test/java/org/redi_school/attendance/FeatureTest.java
+++ src/test/java/org/redi_school/attendance/FeatureTest.java
@@ -1,13 +1,17 @@
 package org.redi_school.attendance;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.fluentlenium.adapter.junit.FluentTest;
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import org.fluentlenium.adapter.FluentAdapter;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.service.DriverService;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,21 +21,35 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @RunWith(SpringJUnit4ClassRunner.class)
-public class FeatureTest extends FluentTest {
+public class FeatureTest extends FluentAdapter {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     @LocalServerPort
     private int port;
 
     @BeforeClass
     public static void setupClass() {
-        ChromeDriverManager.getInstance().setup();
+        PhantomJsDriverManager.getInstance().setup();
     }
 
     @Before
     public void setUp() throws Exception {
-        driver = new ChromeDriver();
+        if (null == driver) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            DriverService service = PhantomJSDriverService.createDefaultService(capabilities);
+
+            driver = new PhantomJSDriver(service, capabilities);
+        }
+
+        initFluent(driver);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
