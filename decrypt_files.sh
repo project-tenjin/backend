18b1ef78e2f60d706be00e90ca614ebfaf1760aa
Svetlana <svetlana.fayustova@gmail.com>
2023-01-14 14:29:47 +0100

    fix properties decryption

--- decrypt_files.sh
+++ decrypt_files.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-travis encrypt-file -d --key $KEY_FROM_LASTPASS --iv $IV_FROM_LASTPASS src/main/resources/google_sheets_credentials.json.enc src/main/resources/google_sheets_credentials.json
-travis encrypt-file -d --key $KEY_FROM_LASTPASS --iv $IV_FROM_LASTPASS src/main/resources/google_sheets_acceptance_credentials.json.enc src/main/resources/google_sheets_acceptance_credentials.json
-travis encrypt-file -d --key $KEY_FROM_LASTPASS --iv $IV_FROM_LASTPASS src/main/resources/application-production.yml.enc src/main/resources/application-production.yml
-travis encrypt-file -d --key $KEY_FROM_LASTPASS --iv $IV_FROM_LASTPASS src/main/resources/application.yml.enc src/main/resources/application.yml
-travis encrypt-file -d --key $KEY_FROM_LASTPASS --iv $IV_FROM_LASTPASS src/test/resources/application.yml.enc src/test/resources/application.yml
+openssl aes-256-cbc -K $KEY_FROM_LASTPASS -iv $IV_FROM_LASTPASS -in src/main/resources/google_sheets_credentials.json.enc -out src/main/resources/google_sheets_credentials.json -d
+openssl aes-256-cbc -K $KEY_FROM_LASTPASS -iv $IV_FROM_LASTPASS -in src/main/resources/google_sheets_acceptance_credentials.json.enc -out src/main/resources/google_sheets_acceptance_credentials.json -d
+openssl aes-256-cbc -K $KEY_FROM_LASTPASS -iv $IV_FROM_LASTPASS -in src/main/resources/application-production.yml.enc -out src/main/resources/application-production.yml -d
+openssl aes-256-cbc -K $KEY_FROM_LASTPASS -iv $IV_FROM_LASTPASS -in src/main/resources/application.yml.enc -out src/main/resources/application.yml -d
+openssl aes-256-cbc -K $KEY_FROM_LASTPASS -iv $IV_FROM_LASTPASS -in src/test/resources/application.yml.enc -out src/test/resources/application.yml -d
