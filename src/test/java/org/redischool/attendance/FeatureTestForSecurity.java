cccc7f63340dd40cb08406af4d1fa342b8672cef
Jacek Bilski <jacek@bilski.tech>
2023-01-17 22:40:56 +0100

    Reviving Java tests. All are green, except for FeatureTestForSecurity, which I marked with `@Ignore` for now.

--- src/test/java/org/redischool/attendance/FeatureTestForSecurity.java
+++ src/test/java/org/redischool/attendance/FeatureTestForSecurity.java
@@ -2,6 +2,7 @@ package org.redischool.attendance;
 
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -17,98 +18,99 @@ import java.net.URLEncoder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
-//@SpringBootTest(
-//        webEnvironment = DEFINED_PORT,
-//        properties = {
-//                "credentials.username=foo",
-//                "credentials.password=bar"
-//        }
-//)
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ActiveProfiles("cloud")
-//public class FeatureTestForSecurity extends FeatureTestScaffolding {
-//
-//    // redi-project-tenjin@googlegroups.com has access to Chasing Unicorns but not to Android App. Configured in okta admin console with group settings
-//    private static final String COURSE_NAME_I_CANNOT_ACCESS = "Android App";
-//
-//    private static WebDriver driver;
-//
-//    @Value("${okta.test.username}")
-//    private String username;
-//
-//    @Value("${okta.test.password}")
-//    private String password;
-//
-//    @Before
-//    public void setUp() {
-//        boolean firstRun = driver == null;
-//
-//        if (firstRun) {
-//            driver = createWebDriver();
-//        }
-//
-//        initFluent(driver);
-//        setBaseUrl("http://localhost:" + port + "/");
-//    }
-//
-//    @AfterClass
-//    public static void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-//
-//    @Test
-//    public void testSeesOnlyAssignedCourses() {
-//        // ACT
-//        goHome();
-//
-//        // ASSERT
-//        assertThat(isCourseVisible(COURSE_NAME)).isTrue();
-//        assertThat(isCourseVisible(COURSE_NAME_I_CANNOT_ACCESS)).isFalse();
-//    }
-//
-//    @Test
-//    public void testHasAccessOnlyToCoursesAssigned() {
-//        // ARRANGE
-//        goHome();
-//
-//        // ACT
-//        selectCourse(COURSE_NAME);
-//
-//        // ASSERT
-//        assertThat($("h1#courseName").first().text()).isEqualTo(COURSE_NAME);
-//    }
-//
-//    @Test
-//    public void testHasNoAccessToCoursersNotAssigned() throws UnsupportedEncodingException {
-//        // ARRANGE
-//        goHome();
-//
-//        // ACT
-//        goTo(getBaseUrl() + "courses?name=" + URLEncoder.encode(COURSE_NAME_I_CANNOT_ACCESS, "utf-8"));
-//
-//        // ASSERT
-//        assertThat($("h1").first().text()).isEqualTo("You do not have permissions to view this course.");
-//    }
-//
-//    private void goHome() {
-//        goTo(getBaseUrl());
-//
-//        if (!authenticated()) {
-//            loginViaOkta();
-//        }
-//    }
-//
-//    private boolean authenticated() {
-//        return !url().contains("/login");
-//    }
-//
-//    private void loginViaOkta() {
-//        $("#okta-signin-username").fill().withText(username);
-//        $("#okta-signin-password").fill().withText(password);
-//        $("#okta-signin-submit").submit();
-//        WebDriverWait wait = new WebDriverWait(driver, 10);
-//        wait.until((fluentControl) -> driver.getCurrentUrl().contains("localhost"));
-//    }
-//}
+@SpringBootTest(
+        webEnvironment = DEFINED_PORT,
+        properties = {
+                "credentials.username=foo",
+                "credentials.password=bar"
+        }
+)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("cloud")
+@Ignore("Currently failing, investigation in progress")
+public class FeatureTestForSecurity extends FeatureTestScaffolding {
+
+    // redi-project-tenjin@googlegroups.com has access to Chasing Unicorns but not to Android App. Configured in okta admin console with group settings
+    private static final String COURSE_NAME_I_CANNOT_ACCESS = "Android App";
+
+    private static WebDriver driver;
+
+    @Value("${okta.test.username}")
+    private String username;
+
+    @Value("${okta.test.password}")
+    private String password;
+
+    @Before
+    public void setUp() {
+        boolean firstRun = driver == null;
+
+        if (firstRun) {
+            driver = createWebDriver();
+        }
+
+        initFluent(driver);
+        setBaseUrl("http://localhost:" + port + "/");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testSeesOnlyAssignedCourses() {
+        // ACT
+        goHome();
+
+        // ASSERT
+        assertThat(isCourseVisible(COURSE_NAME)).isTrue();
+        assertThat(isCourseVisible(COURSE_NAME_I_CANNOT_ACCESS)).isFalse();
+    }
+
+    @Test
+    public void testHasAccessOnlyToCoursesAssigned() {
+        // ARRANGE
+        goHome();
+
+        // ACT
+        selectCourse(COURSE_NAME);
+
+        // ASSERT
+        assertThat($("h1#courseName").first().text()).isEqualTo(COURSE_NAME);
+    }
+
+    @Test
+    public void testHasNoAccessToCoursersNotAssigned() throws UnsupportedEncodingException {
+        // ARRANGE
+        goHome();
+
+        // ACT
+        goTo(getBaseUrl() + "courses?name=" + URLEncoder.encode(COURSE_NAME_I_CANNOT_ACCESS, "utf-8"));
+
+        // ASSERT
+        assertThat($("h1").first().text()).isEqualTo("You do not have permissions to view this course.");
+    }
+
+    private void goHome() {
+        goTo(getBaseUrl());
+
+        if (!authenticated()) {
+            loginViaOkta();
+        }
+    }
+
+    private boolean authenticated() {
+        return !url().contains("/login");
+    }
+
+    private void loginViaOkta() {
+        $("#okta-signin-username").fill().withText(username);
+        $("#okta-signin-password").fill().withText(password);
+        $("#okta-signin-submit").submit();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until((fluentControl) -> driver.getCurrentUrl().contains("localhost"));
+    }
+}
--- src/test/java/org/redischool/attendance/details/OktaGroupsCourseAccessValidatorTest.java
+++ src/test/java/org/redischool/attendance/details/OktaGroupsCourseAccessValidatorTest.java
@@ -15,55 +15,55 @@ import java.util.HashMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-//@RunWith(MockitoJUnitRunner.class)
-//public class OktaGroupsCourseAccessValidatorTest {
-//    @Mock
-//    private Authentication authentication;
-//
-//    private OktaGroupsCourseAccessValidator courseAccessValidator;
-//
-//    @Before
-//    public void setUp() {
-//        courseAccessValidator = new OktaGroupsCourseAccessValidator(new ObjectMapper());
-//    }
-//
-//    @Test
-//    public void validatePermissions_doesNotThrow_whenUserHasPermissions() throws UserAccessDeniedException {
-//        HashMap<Object, Object> authDetails = new HashMap<>();
-//        authDetails.put("tokenValue", tokenWithGroup("Chasing Unicorns"));
-//        when(authentication.getDetails()).thenReturn(authDetails);
-//
-//        courseAccessValidator.validatePermissions(authentication, "Chasing Unicorns");
-//    }
-//
-//    @Test(expected = UserAccessDeniedException.class)
-//    public void validatePermissions_throws_whenUserDoesNotHavePermissions() throws UserAccessDeniedException {
-//        HashMap<Object, Object> authDetails = new HashMap<>();
-//        authDetails.put("tokenValue", tokenWithGroup("Chasing Unisus"));
-//        when(authentication.getDetails()).thenReturn(authDetails);
-//
-//        courseAccessValidator.validatePermissions(authentication, "Chasing Unicorns");
-//    }
-//
-//    @Test
-//    public void hasPermissions_returnsTrue_whenUserHasPermissions() {
-//        HashMap<Object, Object> authDetails = new HashMap<>();
-//        authDetails.put("tokenValue", tokenWithGroup("Chasing Unicorns"));
-//        when(authentication.getDetails()).thenReturn(authDetails);
-//
-//        assertThat(courseAccessValidator.hasPermissions(authentication, "Chasing Unicorns")).isTrue();
-//    }
-//
-//    @Test
-//    public void hasPermissions_returnsFalse_whenUserDoesNotHavePermissions() {
-//        HashMap<Object, Object> authDetails = new HashMap<>();
-//        authDetails.put("tokenValue", tokenWithGroup("Chasing Unisus"));
-//        when(authentication.getDetails()).thenReturn(authDetails);
-//
-//        assertThat(courseAccessValidator.hasPermissions(authentication, "Chasing Unicorns")).isFalse();
-//    }
-//
-//    private String tokenWithGroup(String groupName) {
-//        return JwtHelper.encode("{\"groups\":[\"" + groupName + "\"]}", new MacSigner("FOO")).getEncoded();
-//    }
-//}
+@RunWith(MockitoJUnitRunner.class)
+public class OktaGroupsCourseAccessValidatorTest {
+    @Mock
+    private Authentication authentication;
+
+    private OktaGroupsCourseAccessValidator courseAccessValidator;
+
+    @Before
+    public void setUp() {
+        courseAccessValidator = new OktaGroupsCourseAccessValidator(new ObjectMapper());
+    }
+
+    @Test
+    public void validatePermissions_doesNotThrow_whenUserHasPermissions() throws UserAccessDeniedException {
+        HashMap<Object, Object> authDetails = new HashMap<>();
+        authDetails.put("tokenValue", tokenWithGroup("Chasing Unicorns"));
+        when(authentication.getDetails()).thenReturn(authDetails);
+
+        courseAccessValidator.validatePermissions(authentication, "Chasing Unicorns");
+    }
+
+    @Test(expected = UserAccessDeniedException.class)
+    public void validatePermissions_throws_whenUserDoesNotHavePermissions() throws UserAccessDeniedException {
+        HashMap<Object, Object> authDetails = new HashMap<>();
+        authDetails.put("tokenValue", tokenWithGroup("Chasing Unisus"));
+        when(authentication.getDetails()).thenReturn(authDetails);
+
+        courseAccessValidator.validatePermissions(authentication, "Chasing Unicorns");
+    }
+
+    @Test
+    public void hasPermissions_returnsTrue_whenUserHasPermissions() {
+        HashMap<Object, Object> authDetails = new HashMap<>();
+        authDetails.put("tokenValue", tokenWithGroup("Chasing Unicorns"));
+        when(authentication.getDetails()).thenReturn(authDetails);
+
+        assertThat(courseAccessValidator.hasPermissions(authentication, "Chasing Unicorns")).isTrue();
+    }
+
+    @Test
+    public void hasPermissions_returnsFalse_whenUserDoesNotHavePermissions() {
+        HashMap<Object, Object> authDetails = new HashMap<>();
+        authDetails.put("tokenValue", tokenWithGroup("Chasing Unisus"));
+        when(authentication.getDetails()).thenReturn(authDetails);
+
+        assertThat(courseAccessValidator.hasPermissions(authentication, "Chasing Unicorns")).isFalse();
+    }
+
+    private String tokenWithGroup(String groupName) {
+        return JwtHelper.encode("{\"groups\":[\"" + groupName + "\"]}", new MacSigner("FOO")).getEncoded();
+    }
+}
